cfa47a7c981e99abeeb4c46aa15ac2d1d99ef9b9
zeeeeel <zziyeliu@gmail.com>
2023-01-29 21:06:27 -0800

    Multiple updates - overhaul of the code

--- playground.py
+++ playground.py
@@ -2,11 +2,23 @@ import json
 import os
 import threading
 import tkinter as tk
-
+from tkinter import messagebox, simpledialog
+from pydub import AudioSegment
 import pyperclip
-
+import sqlite3
 import openai
+import subprocess
+import atexit
+import queue
+import sys
+from time import sleep
 
+
+# import transcribe
+
+def on_closing():
+    root.destroy()
+    transcribe_action.transcribe_stop()
 
 def on_submit(event=None):
 
@@ -29,10 +41,116 @@ def on_stop():
 def on_copy_to_input():
     output_text.clipboard_clear()
     output_text.clipboard_append(output_text.get("1.0", 'end-1c'))
-    input_text.insert('end', "\n\nResponse " + output_text.get("1.0", 'end-1c'))
+    # print Response: if output does not contain it as a substring
+    if "Response:" not in output_text.get("1.0", 'end-1c'):
+        input_text.insert('end', "\n\nResponse: " + output_text.get("1.0", 'end-1c'))
 
-def on_copy_to_clipboard():
+def on_copy_input_to_clipboard():
+    pyperclip.copy(input_text.get("1.0", 'end-1c'))
+
+def on_copy_output_to_clipboard():
     pyperclip.copy(output_text.get("1.0", 'end-1c'))
+
+class TranscribeAction:
+    def __init__(self):
+        self.transcribe_Thread = None
+        self.transcribe_started = False
+
+    def transcribe_start(self):
+        self.transcribe_started = False
+        # Clear the input and outtext
+        input_text.delete("1.0", tk.END)
+        output_text.delete("1.0", tk.END)
+
+        # Ask for confirmation
+        result = messagebox.askokcancel("Transcribe Session", "Do you want to start a transcribe session?")
+        if result == True:
+            # Get the name of the transcribe session
+            session_name = simpledialog.askstring("Transcribe Session", "Please enter the name of the transcribe session:")
+            # Create and start a new thread to stream the completions
+            self.transcribe_Thread = TranscribeThread(session_name)
+            self.transcribe_Thread.start()
+        self.transcribe_started = True
+
+    def transcribe_stop(self):
+        if self.transcribe_started:
+            self.transcribe_Thread.stop()
+                # Directory where the .wav files are located
+            directory = os.path.abspath('/Users/ziyeliu/Code/play_ground/audio_recording_temp/')
+
+            # Initialize an empty audio file
+            consolidated_audio = AudioSegment.empty()
+
+            # Iterate through all .wav files in the directory
+            for filename in os.listdir(directory):
+                if filename.endswith('.wav'):
+                    # Load the audio file
+                    audio = AudioSegment.from_file(os.path.join(directory, filename))
+                    # Append the audio file to the consolidated audio
+                    consolidated_audio += audio
+
+            # Export the consolidated audio to a new file
+            consolidated_audio.export("audio_recording/consolidated_audio.wav", format="wav")
+            # Delete delete temp folder
+            os.system("rm -r audio_recording_temp")
+            
+transcribe_action = TranscribeAction()
+
+class TranscribeThread(threading.Thread):
+    def __init__(self, session_name):
+        super().__init__()
+        self.stopped = False
+        self.output_queue = queue.Queue() 
+        # remove all instance of '`' in session_name
+        session_name = session_name.replace('`', '')
+        # print current  timestamp in string forma
+        from datetime import datetime
+        self.session_name = session_name
+        self.timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        self.transcribe_process = None
+
+    
+    def run(self):
+        self.transcribe_process = subprocess.Popen([sys.executable, '/Users/ziyeliu/Code/play_ground/transcribe.py', '--model', 'small', '--pause', '0.6', '--session_name', self.session_name, '--session_timestamp', self.timestamp], stderr=subprocess.PIPE) 
+
+        atexit.register(self.stop)
+
+        # Get the output from the other program
+        last_checked_timestamp = 0
+        conn = sqlite3.connect(os.path.abspath('/Users/ziyeliu/Code/play_ground/database/transcribe.db')) 
+        while True:
+            # Check for new changes
+            try:
+                cursor = conn.execute(f"SELECT timestamp, session_time, transcribed FROM transcribe_session WHERE session_time = '{self.timestamp}' and timestamp > {last_checked_timestamp}")
+            except sqlite3.OperationalError:
+                #print("Waiting for first transcript")
+                sleep(0.1)
+                continue
+            rows = cursor.fetchall()
+            if len(rows)>0:
+                # get the last timestamp from all the rows
+                rows.sort(key=lambda x: x[0])
+                if len(rows) > 0:
+                    for row in rows:
+                        row = list(row)
+                        # Check to see if the last 3 characters are '...' and if so, remove them
+                        if row[2][-3:] == '...':
+                            row[2] = row[2][:-3]
+                            output_text.insert('end', "- " + row[2])
+                        # print the text
+                        print(row[2])
+                        # add the text to the output queue
+                        output_text.insert('end', "- " + row[2] + '\n')
+                last_checked_timestamp = rows[-1][0]
+            sleep(0.1)
+
+    def stop(self):
+        # stop if run() no longer executing
+        self.stopped = True
+        # terminate the process if it is still running
+        if self.transcribe_process and self.transcribe_process.poll() is None:
+            self.transcribe_process.terminate()
+        print("Transcribe session stopped")
 
 class StreamingThread(threading.Thread):
     def __init__(self, prompt, selected_template, echo_check):
@@ -63,7 +181,7 @@ class StreamingThread(threading.Thread):
             if self.echo_check:
                 output_text.insert("end", self.prompt)
             for completion in completions:
-                if not self.stopped and not self.stopped:
+                if not self.stopped:
                     # Insert the completed text into the output text widget
                     output_text.insert('end', completion['choices'][0]['text'])
                     output_text.see('end')
@@ -85,7 +203,6 @@ openai.api_key = os.environ.get('OpenAIToken')
 # Create a new Tkinter window
 root = tk.Tk()
 root.title("OpenAI Text Completion")
-
 
 # Create a Label for the template selection
 template_label = tk.Label(root, text="Select template:")
@@ -134,13 +251,25 @@ copy_button = tk.Button(user_input_frame, text="Copy to input field", command=on
 copy_button.pack(side = 'left')
 
 # Create a Button to copy the output text to clipboard
-copy_button = tk.Button(user_input_frame, text="Copy to clipboard", command=on_copy_to_clipboard)
+copy_button = tk.Button(user_input_frame, text="Copy to input to clipboard", command=on_copy_input_to_clipboard)
 copy_button.pack(side = 'left')
 
 # Create a checkbox to enable output_text to be prefixed with the content of input_text
 echo_check = tk.BooleanVar()
 echo_check_box = tk.Checkbutton(user_input_frame, text="Echo", variable=echo_check)
 echo_check_box.pack(side = 'left')
+
+# Create transcribe frame
+transcribe_frame = tk.Frame(root)
+transcribe_frame.pack()
+
+# Create a Button to start transcribe
+transcribe_start_button = tk.Button(transcribe_frame, text="Start transcribe", command=transcribe_action.transcribe_start)
+transcribe_start_button.pack(side = 'left')
+
+# Create a Button to stop transcribe
+transcribe_stop_button = tk.Button(transcribe_frame, text="Stop transcribe", command=transcribe_action.transcribe_stop)
+transcribe_stop_button.pack(side = 'left')
 
 # Create a Label for the output text
 output_label = tk.Label
@@ -152,8 +281,16 @@ output_label.pack()
 output_text = tk.Text(root, height=25, width=150)
 output_text.pack()
 
+# Create output frame
+user_output_frame = tk.Frame(root)
+user_output_frame.pack()
+
+# Create a Button to copy the output text to clipboard
+copy_button = tk.Button(user_output_frame, text="Copy to output to clipboard", command=on_copy_output_to_clipboard)
+copy_button.pack(side = 'left')
+
 # Bind shift-enter to submit button
 root.bind('<Shift-Return>', on_submit)
-
+root.protocol("WM_DELETE_WINDOW", on_closing)
 # Run the Tkinter event loop
 root.mainloop()
---
+++ transcribe.py
@@ -0,0 +1,138 @@
+import io
+from time import sleep, time
+import speech_recognition as sr
+import whisper
+import queue
+import tempfile
+import os
+import threading
+import click
+import torch
+import numpy as np
+import warnings
+import queue
+import sqlite3
+import datetime
+import wave
+
+
+global running_recording 
+running_recording = True
+
+
+
+@click.command()
+@click.option("--model", default="base", help="Model to use", type=click.Choice(["tiny","base", "small","medium","large"]))
+@click.option("--english", default=True, help="Whether to use English model",is_flag=True, type=bool)
+@click.option("--verbose", default=False, help="Whether to print verbose output", is_flag=True,type=bool)
+@click.option("--energy", default=300, help="Energy level for mic to detect", type=int)
+@click.option("--dynamic_energy", default=False,is_flag=True, help="Flag to enable dynamic engergy", type=bool)
+@click.option("--pause", default=0.8, help="Pause time before entry ends", type=float)
+@click.option("--save_entire_clip",default=True, help="Flag to save entire clip", is_flag=True,type=bool)
+@click.option("--session_name", default="", help="Name of the session", type=str)
+@click.option("--session_timestamp", default="", help="Timestamp of the session", type=str)
+def main(model, english,verbose, energy, pause,dynamic_energy, save_entire_clip, session_name, session_timestamp):
+    # Check timestamp format and make sure it is "%Y_%m_%d_%H_%M_%S"
+    try:
+        datetime.datetime.strptime(session_timestamp, '%Y_%m_%d_%H_%M_%S') 
+    except ValueError:
+        print("Incorrect data format, should be YYYY_MM_DD_HH_MM_SS")
+        return
+    # Terminate if session_name is not provided
+    if session_name == "":
+        print("Session name and timestamp are required")
+    os.makedirs(os.path.abspath('/Users/ziyeliu/Code/play_ground/database/'), exist_ok=True)
+    os.makedirs(os.path.abspath('/Users/ziyeliu/Code/play_ground/audio_recording/'), exist_ok=True)
+    os.makedirs(os.path.abspath('/Users/ziyeliu/Code/play_ground/audio_recording_temp/'), exist_ok=True)
+
+    # Create sqlite3 database called transcribe.db with 3 columns: date, timestamp
+    conn = sqlite3.connect(os.path.abspath('/Users/ziyeliu/Code/play_ground/database/transcribe.db')) 
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS transcribe (session_time text NOT NULL PRIMARY KEY UNIQUE, session_name text, session_text text)''')
+    conn.commit()
+    # Add timestamp to the following create table statement
+    c.execute(f"CREATE TABLE IF NOT EXISTS {f'transcribe_session'} (timestamp INTEGER, session_time text, transcribed text)")
+    c.execute("CREATE INDEX IF NOT EXISTS timestamp_idx ON transcribe_session (session_time)")
+    conn.commit()
+
+    #there are no english models for large
+    if model != "large" and english:
+        model = model + ".en"
+    audio_model = whisper.load_model(model)
+    audio_queue = queue.Queue()
+    record_thread = threading.Thread(target=record_audio,
+                     args=(audio_queue, energy, pause, dynamic_energy))
+    #record_thread.setDaemon(True)
+    record_thread.start()
+    # Add session entree to transcribe database
+    c.execute(f"INSERT INTO transcribe VALUES ('{session_timestamp}', '{session_name}', 'NULL')")
+    transcribe_thread = threading.Thread(target=transcribe_forever,
+                     args=(audio_queue, audio_model, english, verbose, session_name, session_timestamp))
+    #transcribe_thread.setDaemon(True)
+    transcribe_thread.start()
+    c.close()
+    conn.close()
+
+# specify parse transcribe database to list out the available sessions for the day
+def list_session_of_the_day(year_month_date) -> list:
+    conn = sqlite3.connect('transcribe.db')
+    c = conn.cursor()
+    c.execute(f"SELECT * FROM transcribe WHERE date = '{year_month_date}'")
+    result = c.fetchall()
+    return result
+
+def record_audio(audio_queue, energy, pause, dynamic_energy):
+    #load the speech recognizer and set the initial energy threshold and pause threshold
+    r = sr.Recognizer()
+    r.energy_threshold = energy
+    r.pause_threshold = pause
+    r.dynamic_energy_threshold = dynamic_energy
+
+    with sr.Microphone(sample_rate=16000) as source:
+        print("Transcription start:\n")
+        i = 0
+        while True:
+            audio = r.listen(source)
+            #get and save audio to wav file
+            wav_file = wave.open(os.path.abspath('/Users/ziyeliu/Code/play_ground/audio_recording_temp/') + '/audio_' + str(i) + '.wav', 'wb')
+            wav_file.setnchannels(1)
+            wav_file.setsampwidth(2)
+            wav_file.setframerate(16000)
+            wav_file.writeframes(audio.get_raw_data())
+            wav_file.close()
+
+            torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
+            audio_data = torch_audio
+            audio_queue.put_nowait(audio_data)
+            i += 1
+
+def transcribe_forever(audio_queue, audio_model, english, verbose, session_name, session_timestamp):
+    # Create entry in transcribe database 
+
+    # Create sqlite3 database to store a transcribe session with file name of timestamp of session
+    conn = sqlite3.connect(os.path.abspath('/Users/ziyeliu/Code/play_ground/database/transcribe.db'))
+    c = conn.cursor()
+    # Add timestamp to the following create table statement
+    c.execute(f"""INSERT INTO transcribe_session VALUES ("{int(time())}", "{session_timestamp}", "Start of Transcription Session:")""")
+    conn.commit()
+    while True:
+        try:
+            current_timestamp = int(time())
+            audio_data = audio_queue.get()
+            result = audio_model.transcribe(audio_data,language='english')
+
+            if not verbose:
+                predicted_text = result["text"]
+            else:
+                predicted_text = result["text"]
+            if predicted_text != " Thank you.":
+                # generate current integer timestamp
+                # Add entry to transcribe_session database with timestamp, date, session_times, predicted_text
+                c.execute(f"""INSERT INTO transcribe_session VALUES ("{current_timestamp}", "{session_timestamp}", "{predicted_text}")""")
+                conn.commit()
+        except Exception as e:
+            # Log exception to transcribe_session database
+            c.execute(f"""INSERT INTO transcribe_session VALUES ("{current_timestamp}", "{session_timestamp}", "{e}")""")
+            conn.commit()
+
+main()
